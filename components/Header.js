449c08feb1b242920fb0911a17acca0d151ffc94
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-04 21:20:49 +0100

    search page component

---
+++ components/Header.js
@@ -0,0 +1,55 @@
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid"
+import Image from "next/image"
+import { useRouter } from "next/router"
+import { useRef } from "react"
+import Avatar from "./Avatar"
+
+function Header() {
+  const router = useRouter()
+  const searchInputRef = useRef(null)
+
+  const search = (e) => {
+    e.preventDefault()
+    const term = searchInputRef.current.value
+
+    if (!term) return
+
+    router.push(`/search?term=${term}`)
+  }
+
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+          width={120}
+          height={40}
+          className="cursor-pointer"
+          onClick={() => router.push("/")}
+        />
+        <form className="flex flex-grow border border-gray-200 rounded-full shadow-lg max-w-3xl items-center px-6 py-3 ml-10 mr-5">
+          <input
+            type="text"
+            ref={searchInputRef}
+            className="flex-grow w-full focus:outline-none"
+          />
+          <XIcon
+            className="h-7 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125 sm:mr-3"
+            onClick={() => (searchInputRef.current.value = "")}
+          />
+          <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+          <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
+          <button hidden type="submit" onClick={search}>
+            Search
+          </button>
+        </form>
+        <Avatar
+          className="ml-auto"
+          url="https://res.cloudinary.com/odd-developer/image/upload/v1627925104/P8iXlJ7x_400x400_f7xbus.jpg"
+        />
+      </div>
+    </header>
+  )
+}
+
+export default Header
