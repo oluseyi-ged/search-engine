c4a855bae7d6bf93313c59d317d89aacd17da479
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-04 22:57:07 +0100

    display search result implemented

---
+++ Response.js
@@ -0,0 +1,317 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - react js",
+        totalResults: "6",
+        searchTerms: "react js",
+        count: 6,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "c52ff557007e2a126",
+      },
+    ],
+  },
+  context: {
+    title: "Google",
+  },
+  searchInformation: {
+    searchTime: 0.782661,
+    formattedSearchTime: "0.78",
+    totalResults: "6",
+    formattedTotalResults: "6",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "How to setup Firebase Authentication with React in 5 minutes ...",
+      htmlTitle:
+        "How to setup Firebase Authentication with <b>React</b> in 5 minutes ...",
+      link: "https://www.google.com/appserve/mkt/p/AFnwnKWM33pYjSykKgZr7yc_MycERBTifImuQ899hlFIBC1nVGyAfDRP2k7UZXVIXpkDZtjtd7ORj9NcHd2jTnD3453e8dSeU9gxNEMt1W93tTx9X2stOf-emlmnLTmlAhRLmRVtlfTTWaaYbxEf2CWUXUfgiwiFjNpzT7ZE8MIKDJ50JBk1xbT8uDPtht66A4wycUcHLkMF2mE8AQW_2DWNGCdzBQNdCp13wn0o7tRhLgafOYU1b-ueetlTQvlqUUhUe0TgjSsZKGg1S6nDPnuHqdQ",
+      displayLink: "www.google.com",
+      snippet:
+        "Feb 21, 2019 ... We'll modify the src/App.js component. Insert these four imports to the top of the file: import withFirebaseAuth from 'react-with-firebase-auth'",
+      htmlSnippet:
+        "Feb 21, 2019 <b>...</b> We&#39;ll modify the src/App.<b>js</b> component. Insert these four imports to the top of the file: import withFirebaseAuth from &#39;<b>react</b>-with-firebase-auth&#39;",
+      cacheId: "l6cg7ivnlJYJ",
+      formattedUrl:
+        "https://www.google.com/.../AFnwnKWM33pYjSykKgZr7yc_ MycERBTifImuQ899hlFIBC1nVGyAfDRP2k7UZXVIXpkDZtjtd7ORj9Nc...",
+      htmlFormattedUrl:
+        "https://www.google.com/.../AFnwnKWM33pYjSykKgZr7yc_ MycERBTifImuQ899hlFIBC1nVGyAfDRP2k7UZXVIXpkDZtjtd7ORj9Nc...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTgfzTFmT273jALydgSGFlh1_pGyhX5PDxcMHVEgVEf2wgO4Vrn4YKm1dY",
+            width: "306",
+            height: "165",
+          },
+        ],
+        metatags: [
+          {
+            "twitter:app:url:iphone": "medium://p/bb8bb53e8834",
+            "og:image":
+              "https://miro.medium.com/max/1200/1*PR3klDbot3ZS9Kp69LYtHA.png",
+            "theme-color": "#000000",
+            "article:published_time": "2019-12-12T11:32:00.080Z",
+            "twitter:card": "summary_large_image",
+            "og:site_name": "Medium",
+            "al:android:package": "com.medium.reader",
+            "twitter:label1": "Reading time",
+            "twitter:app:id:iphone": "828256236",
+            title:
+              "How to setup Firebase Authentication with React in 5 minutes (maybe 10) | by Armando Magalhães | Firebase Developers | Medium",
+            "al:ios:url": "medium://p/bb8bb53e8834",
+            "og:description":
+              "A quick guide on how you can implement Firebase Authentication easily in a React application",
+            "twitter:creator": "@armand1m",
+            "al:ios:app_store_id": "828256236",
+            "twitter:data1": "4 min read",
+            "twitter:site": "@firebase",
+            "og:type": "article",
+            "twitter:title":
+              "How to setup Firebase Authentication with React in 5 minutes (maybe 10)",
+            "al:ios:app_name": "Medium",
+            author: "Armando Magalhães",
+            "og:title":
+              "How to setup Firebase Authentication with React in 5 minutes (maybe 10)",
+            "al:web:url":
+              "https://medium.com/firebase-developers/how-to-setup-firebase-authentication-with-react-in-5-minutes-maybe-10-bb8bb53e8834",
+            "article:author": "https://armand1m.medium.com",
+            "twitter:image:src":
+              "https://miro.medium.com/max/1200/1*PR3klDbot3ZS9Kp69LYtHA.png",
+            "al:android:url": "medium://p/bb8bb53e8834",
+            referrer: "unsafe-url",
+            "fb:app_id": "542599432471018",
+            viewport: "width=device-width,minimum-scale=1,initial-scale=1",
+            "twitter:description":
+              "A quick guide on how you can implement Firebase Authentication easily in a React application",
+            "og:url":
+              "https://medium.com/firebase-developers/how-to-setup-firebase-authentication-with-react-in-5-minutes-maybe-10-bb8bb53e8834",
+            "twitter:app:name:iphone": "Medium",
+            "al:android:app_name": "Medium",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://miro.medium.com/max/1200/1*PR3klDbot3ZS9Kp69LYtHA.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google",
+      htmlTitle: "Google",
+      link: "https://www.google.com/setprefdomain?prefdom=GE&prev=http://www.google.ge/&sig=K_3qZ97j8Cl4B2sSgi9ME8mvxAE0U%3D",
+      displayLink: "www.google.com",
+      snippet:
+        "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking ...",
+      htmlSnippet:
+        "Search the world&#39;s information, including webpages, images, videos and more. Google has many special features to help you find exactly what you&#39;re looking&nbsp;...",
+      cacheId: "4oh_Sf5JYYEJ",
+      formattedUrl:
+        "https://www.google.com/setprefdomain?prefdom=GE&prev...K...",
+      htmlFormattedUrl:
+        "https://www.google.com/setprefdomain?prefdom=GE&amp;prev...K...",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin",
+            viewport: "width=device-width,minimum-scale=1.0",
+            google: "notranslate",
+            "format-detection": "telephone=no",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Text fields - Material Design",
+      htmlTitle: "Text fields - Material Design",
+      link: "http://www.google.com/design/spec/components/text-fields.html",
+      displayLink: "www.google.com",
+      snippet:
+        "This demo lets you preview the text field component, its variations, and configuration options. Each tab displays a different type of text field.",
+      htmlSnippet:
+        "This demo lets you preview the text field component, its variations, and configuration options. Each tab displays a different type of text field.",
+      cacheId: "58qYSyAQgqQJ",
+      formattedUrl: "www.google.com/design/spec/components/text-fields.html",
+      htmlFormattedUrl:
+        "www.google.com/design/spec/components/text-fields.html",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRKFy49Hon1iZ5ngvxLWCfg72oCvvoDxIk_BZCOW_jSY2pkTfWfQsyYEE5d",
+            width: "318",
+            height: "159",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://lh3.googleusercontent.com/AGfh0Wh3MHvxH-ME7Jc4XOXRwdiKF3DGqE09pnXdmUyNRGSQibwzgGgtbRzNlxjH-Zvn6PzSFmhVCDGry7H2IFuAl_-vnf-zQPryFpA",
+            "twitter:title": "Material Design",
+            "og:type": "article",
+            "twitter:card": "summary_large_image",
+            "theme-color": "#ffffff",
+            "og:site_name": "Material Design",
+            "twitter:url": "https://material.io/components/text-fields",
+            "og:title": "Material Design",
+            "og:description":
+              "Text fields let users enter, edit, and select text.",
+            "twitter:image": "https://material.io/static/assets/result.png",
+            viewport: "initial-scale=1, width=device-width",
+            "twitter:description":
+              "Build beautiful, usable products faster. Material Design is an adaptable system—backed by open-source code—that helps teams build high quality digital experiences.",
+            "format-detection": "telephone=no",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/AGfh0Wh3MHvxH-ME7Jc4XOXRwdiKF3DGqE09pnXdmUyNRGSQibwzgGgtbRzNlxjH-Zvn6PzSFmhVCDGry7H2IFuAl_-vnf-zQPryFpA",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Apps Script – Google Apps Script",
+      htmlTitle: "Apps Script – Google Apps Script",
+      link: "https://www.google.com/script/start/",
+      displayLink: "www.google.com",
+      snippet:
+        "Apps Script is a rapid application development platform that makes it fast and easy to create business applications that integrate with G Suite.",
+      htmlSnippet:
+        "Apps Script is a rapid application development platform that makes it fast and easy to create business applications that integrate with G Suite.",
+      cacheId: "WdtjleQDRPwJ",
+      formattedUrl: "https://www.google.com/script/start/",
+      htmlFormattedUrl: "https://www.google.com/script/start/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw4T7_Oc91AA1xgBHh0AZ-8YeyYvv8rDbFVCHVQpjPVksZ1NEur9NIZo8",
+            width: "292",
+            height: "173",
+          },
+        ],
+        metatags: [
+          {
+            viewport: "initial-scale=1, minimum-scale=1, width=device-width",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.google.com/script/start/as-playvid.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Chrome Browser Privacy Policy - Google Chrome",
+      htmlTitle: "Chrome Browser Privacy Policy - Google Chrome",
+      link: "https://www.google.com/chrome/privacy/",
+      displayLink: "www.google.com",
+      snippet:
+        "Jan 15, 2021 ... Browser modes; Managing users in Chrome; Safe Browsing practices; Privacy practices of using apps, extensions, themes, services, and other add- ...",
+      htmlSnippet:
+        "Jan 15, 2021 <b>...</b> Browser modes; Managing users in Chrome; Safe Browsing practices; Privacy practices of using apps, extensions, themes, services, and other add-&nbsp;...",
+      cacheId: "fcQKQehLxB4J",
+      formattedUrl: "https://www.google.com/chrome/privacy/",
+      htmlFormattedUrl: "https://www.google.com/chrome/privacy/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQKOXFpJVF_Xz2cq8bSojiNKHJUoBzHsj8n3Sdsy1dQnCNO135b0qOVTw",
+            width: "48",
+            height: "48",
+          },
+        ],
+        metatags: [
+          {
+            "msapplication-config":
+              "/chrome/static/images/favicons/browserconfig.xml",
+            "og:image":
+              "https://www.google.com/chrome/static/images/chrome-logo.svg",
+            "twitter:card": "summary_large_image",
+            "twitter:title": "Chrome Browser Privacy Policy - Google Chrome",
+            "og:type": "website",
+            "twitter:url": "https://www.google.com/chrome/privacy/",
+            "og:title": "Chrome Browser Privacy Policy - Google Chrome",
+            "og:description":
+              "Chrome takes your privacy very seriously. Here's everything you need to know about your privacy while browsing the web on Chrome.",
+            "twitter:image":
+              "https://www.google.com/chrome/static/images/chrome-logo.svg",
+            viewport: "width=device-width, initial-scale=1",
+            "twitter:description":
+              "Chrome takes your privacy very seriously. Here's everything you need to know about your privacy while browsing the web on Chrome.",
+            "og:locale": "en_US",
+            "og:url": "https://www.google.com/chrome/privacy/",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.google.com/chrome/static/images/chrome-logo.svg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Understanding motion - Material Design",
+      htmlTitle: "Understanding motion - Material Design",
+      link: "https://www.google.com/design/spec/motion/",
+      displayLink: "www.google.com",
+      snippet:
+        "GitHub · Twitter · YouTube · Blog RSS · Subscribe for updates · Privacy Policy · Terms of Service; Feedback. This website requires JavaScript.",
+      htmlSnippet:
+        "GitHub &middot; Twitter &middot; YouTube &middot; Blog RSS &middot; Subscribe for updates &middot; Privacy Policy &middot; Terms of Service; Feedback. This website requires <b>JavaScript</b>.",
+      cacheId: "3SnFPkBpTzkJ",
+      formattedUrl: "https://www.google.com/design/spec/motion/",
+      htmlFormattedUrl: "https://www.google.com/design/spec/motion/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRZv_x9p1RcvNlSPnmenkZ2A5SbGoXr-IWZcM0L-QvDywCsvTRb75J59Rqz",
+            width: "318",
+            height: "159",
+          },
+        ],
+        metatags: [
+          {
+            "og:image":
+              "https://lh3.googleusercontent.com/w5Ps-MB9QGF0__Pidd8U-Z-LuS6vfPo9lMD_r6l7WGGujA8giU3F860fbmzMV0eqn38tCXSEdksC50W6q6YOzN7OAVAKBPb5Aapf4w",
+            "twitter:title": "Material Design",
+            "og:type": "article",
+            "twitter:card": "summary_large_image",
+            "theme-color": "#ffffff",
+            "og:site_name": "Material Design",
+            "twitter:url":
+              "https://material.io/design/motion/understanding-motion.html",
+            "og:title": "Material Design",
+            "og:description":
+              "Motion helps make a UI expressive and easy to use.",
+            "twitter:image": "https://material.io/static/assets/result.png",
+            viewport: "initial-scale=1, width=device-width",
+            "twitter:description":
+              "Build beautiful, usable products faster. Material Design is an adaptable system—backed by open-source code—that helps teams build high quality digital experiences.",
+            "format-detection": "telephone=no",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://lh3.googleusercontent.com/w5Ps-MB9QGF0__Pidd8U-Z-LuS6vfPo9lMD_r6l7WGGujA8giU3F860fbmzMV0eqn38tCXSEdksC50W6q6YOzN7OAVAKBPb5Aapf4w",
+          },
+        ],
+      },
+    },
+  ],
+}
---
+++ keys.js
@@ -0,0 +1,5 @@
+//API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyDtqP6EVHt9mEyyjOPy0nnEp2AIK3wSYWw"
+
+//Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "c52ff557007e2a126"
